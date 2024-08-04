652d49fd63690f5432ecbf2e3e1d3f26e24d3d82
Edson Villarroel <villarroel.dev@gmail.com>
2024-08-03 22:15:41 -0400

    add animation config

--- rules.mk
+++ rules.mk
@@ -1,5 +1,8 @@
-MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE  = WS2812
+MOUSEKEY_ENABLE = no    # Mouse keys
 OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
+OLED_DRIVER_ENABLE = yes
+# OLED_DRIVER     = ssd1306
 LTO_ENABLE      = yes
+WPM_ENABLE = yes
